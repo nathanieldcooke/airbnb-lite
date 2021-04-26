d6398adf513786390bc2ee52227e11b1f4d33a84
nathanieldcooke <nathanield.cooke@gmail.com>
2021-04-25 22:12:34 -0700

    create log-in drop down

--- frontend/src/components/NavBar/ProfileButton/ProfileButton.js
+++ frontend/src/components/NavBar/ProfileButton/ProfileButton.js
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
+import ProfileBtn from '../../DropDowns/ProfileBtn/ProfileBtn'
 import './ProfileButton.css'
 
 function ProfileButton () {
+
+    let [display, setDisplay] = useState('hidden');
+
+
+    // function setDisplayHelperEnter(e) {
+
+    //     console.log("THis is E:",e.target.className)
+    //     if (e.target.className === 'profile-button-icon-container') {
+    //         setDisplay(display ? '' : 'hidden');
+    //         e.stopPropagation();
+    //         return
+    //     }
+    //     setDisplay('hidden')
+    // }
+
     return (
-        <div className='profile-button-icon-container'>
-            <i className="fas fa-bars"></i>
-            <i className="fas fa-user-circle"></i>
+        <div className='profile-btn-dropdown-conntainer'
+        onClick={((e) => {setDisplay('')})}
+        onMouseLeave={((e) => { setDisplay('hidden') })}
+        >
+            <div className='profile-button-icon-container'>
+                <i className="fas fa-bars"></i>
+                <i className="fas fa-user-circle"></i>
+            </div>
+            {/* <div className='spacer-div'>
+
+            </div> */}
+            <ProfileBtn display={display}/>
         </div>
     )
 }
--- frontend/src/components/NavBar/SearchBar/FormField/FormField.js
+++ frontend/src/components/NavBar/SearchBar/FormField/FormField.js
@@ -1,11 +1,15 @@
+import ProfileBtn from '../../../DropDowns/ProfileBtn/ProfileBtn'
 import './FormField.css'
 
 function FormField ( { cssClassName, text1, text2 } ) {
     return (
-        <div className={`form-field-div ` + cssClassName || '' }>
-            <span>{text1}</span>
-            <span className='span-grey-text'>{text2}</span>
-        </div>
+        <>
+            <div className={`form-field-div ` + cssClassName || '' }>
+                <span>{text1}</span>
+                <span className='span-grey-text'>{text2}</span>
+                <ProfileBtn />
+            </div>
+        </>
     )
 }
 
