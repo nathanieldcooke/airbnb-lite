384036354ac09101deedb1284c76661518cd5933
nathanieldcooke <nathanield.cooke@gmail.com>
2021-04-27 14:43:44 -0700

    create DisplaySpot component's image box

--- frontend/src/components/ShowSpot/DisplaySpot/DisplaySpot.js
+++ frontend/src/components/ShowSpot/DisplaySpot/DisplaySpot.js
@@ -1,30 +1,51 @@
 import './DisplaySpot.css'
 
-const DisplaySpot = () => {
+const DisplaySpot = ( {spot} ) => {
+
+    console.log(spot)
+
+    let title = spot.title
+    let rating = spot.rating
+    // let numOfReviews = null
+    let numOfReviews = spot.Reviews?.length
+    let city = spot.city
+    let state = spot.state
+    let country = spot.country
+    let mainImg = spot.Images?.find(img => img.main === true).src
+    let newImgArr = [];
+    if (spot?.Images)
+    for (let i = 0; i < 4; i++) {
+        newImgArr.push(spot.Images[i].src)
+    }
+    console.log(newImgArr)
+
+    // console.log(mainImg)
+
+
     return (
         <div className='display-spot-container'>
             <div className='display-spot-sub-container'>
                 <div className='display-navbar-space-holder'></div>
                 <div className='display-top-spot-info'>
-                    <span className='display-title'>This Is the Title</span>
+                    <span className='display-title'>{title}</span>
                     <div className='sub-display-top-info'>
-                        <span>@ 4.57</span>
+                        <span><i className="fas fa-star"></i> {rating} <span>({numOfReviews} reviews)</span></span>
+                        <span className='location'>{`${city}, ${state}, ${country}`}</span>
                     </div>
                 </div>
                 <div className='display-images-box'>
-                    <div className='display-main-img'></div>
+                    <div className='display-main-img'><img src={mainImg}></img></div>
                     <div className='display-images-box-sub'>
-                        <img></img>
-                        <img></img>
-                        <img></img>
-                        <img></img>
+                        {newImgArr.map((imgSrc, i) => (
+                            <div key={`spot-div-img${i}`} className={`img${i + 1}`}><img key={`spot-img-img${i}`} src={imgSrc}></img></div>
+                        ))}
                     </div>
-                    <div className='display-spot-info'>
-                        <div className='display-left-info'></div>
-                        <div className='display-right-info'></div>
-                    </div>
-                    <div className='book-button'><button></button></div>
                 </div>
+                <div className='display-spot-info'>
+                    <div className='display-left-info'></div>
+                    <div className='display-right-info'></div>
+                </div>
+                <div className='book-button'><button></button></div>
             </div>
         </div>
     )
--- frontend/src/components/ShowSpot/ShowSpot.js
+++ frontend/src/components/ShowSpot/ShowSpot.js
@@ -10,27 +10,17 @@ import ReviewSpot from './ReviewSpot/ReviewSpot'
 
 const ShowSpot = () => {
     // const dispatch = useDispatch();
-    const spotsArr = useSelector(state => state.spots)
     const spotId = useParams().spotId
     let [spot, setSpot] = useState([])
-    console.log('ShowSpots spotsArr: ', spotsArr)
-    console.log('ShowSpots spotId: ', spotId)
     useEffect( async () => {
         const response = await csrfFetch(`/api/spots/${spotId}`);
         const newSpot = await response.json();
         setSpot(newSpot)
     }, [])
-
-    // const response = await csrfFetch(`/api/spot/${spotId}`);
-    // const spots = await response.json();
-
-    // let spot = spotsArr.find(spot => spot.id === spotId)
-
-
     console.log(spot)
     return (
         <div className='show-spot-container'>
-            <DisplaySpot/>
+            <DisplaySpot spot={spot}/>
             <BookSpot/>
             <ReviewSpot/>
             <div>{spot.id} hello</div>
